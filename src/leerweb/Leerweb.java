4f61e31570725c42a0948ab6bb9bbf6c5aaedf0c
Alberto-Luis15 <Luis_arq8@outlook.es>
2020-06-30 17:55:29 -0500

    Lo que hace este programa es que lee el código fuente de una página web que se actualiza constantemente

---
+++ src/leerweb/Leerweb.java
@@ -0,0 +1,55 @@
+package leerweb;
+
+/**
+ *
+ * @author Luis Alberto Lorenzo Gonzalez
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Leerweb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       leer();
+       
+	}
+ 
+   
+ 
+    public static void leer(){
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+ 
+        try {
+			
+			archivo = new File ("COVID-19 por municipio.html");
+			fr = new FileReader (archivo);
+			br = new BufferedReader(fr);
+ 
+			// Lectura del fichero
+			System.out.println("Leyendo el contendio del archivo web");
+			String linea;
+                        
+			while((linea=br.readLine())!=null)
+				System.out.println(linea);
+        }               
+        catch(Exception e){
+           e.printStackTrace();
+        }finally{
+           
+           try{
+              if( null != fr ){
+                 fr.close();
+              }
+           }catch (Exception e2){
+              e2.printStackTrace();
+           }
+        }
+        System.out.println("Se leyo el archivo web");
+    }
+    }
